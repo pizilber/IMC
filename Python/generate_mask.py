34fc8ef5f785480232fbd6e5e22f0bd057192afa
Pini Zilber <59870145+pizilber@users.noreply.github.com>
2022-02-01 16:02:10 +0200

    Update generate_mask.py

--- Python/generate_mask.py
+++ Python/generate_mask.py
@@ -1,11 +1,10 @@
 import numpy as np
 
-def generate_mask(n1, n2, rank, p):
+def generate_mask(n1, n2, p):
     """
     Generate a mask with probability p for each entry
     :param int n1: number of rows
     :param int n2: number of columns
-    :param int rank: number of minimal observed entries in each row and column
     :param float p: probability of observing an entry
     """
     omega = np.round(0.5 * (np.random.random((n1, n2)) + p))
